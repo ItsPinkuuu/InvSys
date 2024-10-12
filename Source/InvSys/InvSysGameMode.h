d9fe7e82ee222280be1333203444a4e3a469febb
ItsPinkuuu <102356167+ItsPinkuuu@users.noreply.github.com>
2024-10-13 00:44:52 +0200

    Initial commit

---
+++ Source/InvSys/InvSysGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "InvSysGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AInvSysGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AInvSysGameMode();
+};
+
+
+
